f446bdc1559f7013a8e7bd292d14cda78274e381
audrey <audreymateke1@gmail.com>
2024-10-14 00:09:06 +0200

    new file

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,13 +1,15 @@
-from django.urls import path
-from .views import home
-from myapp import views
+from django.urls import path, include
+from django.views.generic import RedirectView
+from . import views  # Import your views
+
 urlpatterns = [
-    path('home/',views.home),
-    path('home/login/',views.user_login,name='login'),
-    path('home/logout/',views.user_logout,name='login'),
-    path('home/register/',views.user_register,name='register'),
-    path('home/add_expences',views.add_expences,name='add_expence'),
-    path('home/delete_expences',views.delete_expences,name='delete_expences'),
-    path('home/update_expences',views.update_expences,name='update_expences'),
-    path('home/update_expences_two',views.update_expences_two,name='update_expences_two'),
+    path('', RedirectView.as_view(url='/home/', permanent=True)),  # Redirect root URL to /home/
+    path('home/', views.home, name='home'),
+    path('home/login/', views.user_login, name='login'),
+    path('home/logout/', views.user_logout, name='logout'),  # Change name to 'logout'
+    path('home/register/', views.user_register, name='register'),
+    path('home/add_expences/', views.add_expences, name='add_expence'),
+    path('home/delete_expences/', views.delete_expences, name='delete_expences'),
+    path('home/update_expences/', views.update_expences, name='update_expences'),
+    path('home/update_expences_two/', views.update_expences_two, name='update_expences_two'),
 ]
